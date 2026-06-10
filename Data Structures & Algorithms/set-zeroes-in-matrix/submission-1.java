625ff54587b3edd9b8452688c851f631145c2fbb
krish236 <75014374+krish236@users.noreply.github.com>
2026-06-11 01:15:38 +0530

    Add: set-zeroes-in-matrix - submission-1

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-1.java
@@ -0,0 +1,61 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        boolean flag1 = false;
+        boolean flag2 = false;
+
+        for(int i = 0; i<n; i++){
+            if(matrix[i][0]==0){
+                flag1 = true;
+                break;
+            }
+        }
+
+        for(int j = 0; j<m; j++){
+            if(matrix[0][j]==0){
+                flag2 = true;
+                break;
+            }
+        }
+
+        for(int i = 1; i<n; i++){
+            for(int j = 1; j<m; j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for(int i = 1; i<n; i++){
+            if(matrix[i][0]==0){
+                for(int j = 1; j<m; j++){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        for(int j = 1; j<m; j++){
+            if(matrix[0][j]==0){
+                for(int i = 1; i<n; i++){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+
+        if(flag1 == true){
+            for(int i = 0; i<n; i++){
+                matrix[i][0] = 0;
+            }
+        }
+
+        if(flag2 == true){
+            for(int j = 0; j<m; j++){
+                matrix[0][j] = 0;
+            }
+        }
+    }
+}
